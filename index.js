2573feab15f4f247cae6a02998b981199000719f
MdMuzaffar <54287360+MdMuzaffar@users.noreply.github.com>
2023-09-09 23:55:23 +0530

    reduce() in JS

--- index.js
+++ index.js
@@ -16,13 +16,44 @@ for (let k = 0; k < numbers.length; k++) {
 
 const fruits = ["apple", "banana", "cherry", "date"];
 for (let l = 0; l < fruits.length; l++) {
-    console.log(fruits[l]);
+    // console.log(fruits[l]);
 }
 
-const num = [1, 2, 3, 4, 5];
+const num = [10, 20, 30, 4, 5];
 
 for (let m = 0; m < num.length; m++) {
-    console.log(num[m])
+    // console.log(num[m])
 }
 
-// forEach()
+
+for (let i = 1; i <= 3; i++) {
+    for (let j = 1; j <= 5; j++) {
+        // console.log(i * j);
+    }
+}
+// Array Elements with reduce:
+
+const numbers1 = [1, 2, 3, 4, 5];
+
+const sum = numbers1.reduce((pre, next) => pre + next, 0);
+// console.log(sum)
+
+const nestedArray = [
+    [1, 2],
+    [3, 4],
+    [5, 6]
+];
+
+const combineNumbers = nestedArray.reduce((acc, total) => acc.concat(total), []);
+console.log(combineNumbers)
+
+// Filtering Arrays with filter:
+
+const evenNumber = numbers1.filter((num) => num % 2 === 0);
+// console.log(evenNumber)
+
+const mySet = new Set([1, 2, 3, 4, 5]);
+
+for (const item of mySet) {
+    // console.log(item);
+}
