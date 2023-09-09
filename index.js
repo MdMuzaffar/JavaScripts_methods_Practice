95c26bd39886d88138ae608805927ec597fa4a19
MdMuzaffar <54287360+MdMuzaffar@users.noreply.github.com>
2023-09-09 23:21:16 +0530

    forLoop Practice

---
+++ index.js
@@ -0,0 +1,28 @@
+// loop
+
+for (let i = 0; i < 10; i++) {
+    // console.log(i)
+}
+
+for (let j = 10; j > 1; j--) {
+    // console.log(j)
+}
+
+const numbers = [10, 20, 30, 40];
+
+for (let k = 0; k < numbers.length; k++) {
+    // console.log(numbers[k])
+}
+
+const fruits = ["apple", "banana", "cherry", "date"];
+for (let l = 0; l < fruits.length; l++) {
+    console.log(fruits[l]);
+}
+
+const num = [1, 2, 3, 4, 5];
+
+for (let m = 0; m < num.length; m++) {
+    console.log(num[m])
+}
+
+// forEach()
